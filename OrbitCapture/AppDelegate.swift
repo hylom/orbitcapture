9b262134b9af0b103210da88393d778201a5bb76
Hiromichi Matsushima <hylom@imac4k.local>
2019-03-01 00:41:13 +0900

    Initial Commit

---
+++ OrbitCapture/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  OrbitCapture
+//
+//  Created by Hiromichi Matsushima on 2019/03/01.
+//  Copyright © 2019年 Hiromichi Matsushima. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
